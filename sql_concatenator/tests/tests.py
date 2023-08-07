3bc918e8fb02633949bfa1e48803460bee8f930e
Kevin Lusignolo <Kevin.Lusignolo@hcss.com>
2023-08-07 13:58:31 -0500

    Add extra unit test for custom concatenation

--- sql_concatenator/tests/tests.py
+++ sql_concatenator/tests/tests.py
@@ -20,6 +20,12 @@ class TestConcatenator(unittest.TestCase):
         actual_output = Concatenator.concatenate(input=input, is_line_split=False, is_custom_split=True, custom_delimiter="123")
         self.assertEqual(actual_output, expected_output)
 
+    def test_concatenate_custom_split_with_space_and_quotes_and_comma(self):
+        input = '798F88D2-0A8C-49BB-AE11-C52B99F410AF", "7D4D9923-003E-4B9D-96C9-A0232CEB1A72", "B5B70DD1-D34F-4494-AE12-59C9F308F001", "6D2A263B-5C8F-41F4-BFBC-623694D671E6'
+        expected_output = "('798F88D2-0A8C-49BB-AE11-C52B99F410AF','7D4D9923-003E-4B9D-96C9-A0232CEB1A72','B5B70DD1-D34F-4494-AE12-59C9F308F001','6D2A263B-5C8F-41F4-BFBC-623694D671E6')"
+        actual_output = Concatenator.concatenate(input=input, is_line_split=False, is_custom_split=True, custom_delimiter='", "')
+        self.assertEqual(actual_output, expected_output)
+
     def test_concatenate_custom_split_does_not_remove_spaces(self):
         input = 'Hello | world | !'
         expected_output = "('Hello ',' world ',' !')"
